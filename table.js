a5a40be96c73c791b380cb140d1247b1ee02f7af
kwadge <47766560+kwadge@users.noreply.github.com>
2023-06-23 09:05:09 -0500

    title change to 'Kwadwo Eck'

---
+++ table.js
@@ -0,0 +1,33 @@
+var mysql = require('mysql2');
+const fs = require('fs');
+
+// Create A Connection
+//var con = mysql.createConnection({
+//  host: "localhost",
+//  user: "root",
+//  password: "password",
+//});
+console.log(__dirname);
+var con = mysql.createConnection({
+  host: 'a5471bb9-c213-4eaf-a9c2-6409bd077891.br37s45d0p54n73ffbr0.databases.appdomain.cloud',
+  user: 'admin',
+  password: '89fc18fa88332a6c0d7b25b76b94b89a2ed590f6c97ab28a4a',
+  port: '30099',
+  database: 'ibmclouddb',
+  ssl: {
+    ca: fs.readFileSync(__dirname + '/48b1ff99-7b37-4370-b9e6-229f4b939777'),
+  }
+});
+
+// Connect To MySQL
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to the Database!");
+
+  // Create Table
+  var sql = 'CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))';
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log('Table has been created...');
+  });
+});
