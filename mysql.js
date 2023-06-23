fb5a344c4900903105bedf0cd4c5640564ca9ca8
kwadge <47766560+kwadge@users.noreply.github.com>
2023-06-23 09:05:50 -0500

    title change

--- mysql.js
+++ mysql.js
@@ -1,13 +1,12 @@
 var mysql = require('mysql2');
 const fs = require('fs');
 
-/* Create A Connection
+// Create A Connection
 //var con = mysql.createConnection({
 //  host: "localhost",
 //  user: "root",
 //  password: "password",
-//}); */
-
+//});
 console.log(__dirname);
 var con = mysql.createConnection({
   host: 'a5471bb9-c213-4eaf-a9c2-6409bd077891.br37s45d0p54n73ffbr0.databases.appdomain.cloud',
@@ -24,45 +23,4 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected to the Database!");
-
-  /* Create Database
-  var sql = 'CREATE DATABASE nodedb';
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log('Database Created!');
-  });*/
-
-  /* Create Table
-  var sql = 'CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))';
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log('Table has been created...');
-  });*/
-
-  /* Alter Table
-  var sql = 'ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY';
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log('Our Table has been altered...');
-  });*/
-
-  /* Insert Into Table
-  var sql = 'INSERT INTO customers (name, email) VALUES ("Mary Smith", "msmith@gmail.com")';
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log('Our Table has been altered...');
-  });*/
-
-  // Insert Many Into Table
-  var sql = 'INSERT INTO customers (name, email) VALUES ?';
-  var values = [
-    ['Tim', 'tim@tim.com'],
-    ['Tina', 'tina@tina.com'],
-    ['Laura', 'laura@laura.com']
-  ];
-  con.query(sql, [values], function (err, result) {
-    if (err) throw err;
-    console.log('Records Inserted: ' + result.affectedRows);
-  });
-
 });
